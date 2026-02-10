3997b342044ed8168720a14abfe9c7e3feb9db72
Hanan Qaisar <hananqaisar316@gmail.com>
2026-02-10 21:52:45 +0500

    Add output function

--- Cpp-OOP-Basics/Type_cunstructor.cpp
+++ Cpp-OOP-Basics/Type_cunstructor.cpp
@@ -18,28 +18,37 @@ class Car
 public:
     string company;
     // 1st type is default in c++:
-    Car(){}//here i write default constructor bcz i create obj like Car car1,car2
-    //2nd type is:
+    Car() {} // here i write default constructor bcz i create obj like Car car1,car2
+    // 2nd type is:
     Car(string name)
     {
         company = name;
     }
-    //3 type is:
-    Car (const Car &carProp){
-        cout<<"This is manually made copy constructor"<<endl;
-        this->company=carProp.company;
+    // 3 type is:
+    Car(const Car &carProp)
+    {
+        cout << "This is manually made copy constructor" << endl;
+        this->company = carProp.company;
     }
-    
+    void output();
 };
+
+void Car ::output()
+{
+    cout << "Company name is : " << company << endl;
+}
 int main()
 {
     int n;
-    Car car1("Toyota"),car2;
-    car2=car1;//This is copy constructor made by me 
-    Car car3(car1);//This is also called copy constructor but in this compiler invokes their own copy constructor
-    Car car4=car1;//here copy constructor is invoked made by me
-    cout<<"Company name is : "<<car1.company<<endl;
-    cout<<"Company name is : "<<car2.company<<endl;
-    cout<<"Company name is : "<<car3.company<<endl;
-    
+    Car car1("Toyota"), car2;
+    car2 = car1;    // This is copy constructor made by me
+    Car car3(car1); // This is also called copy constructor but in this compiler invokes their own copy constructor
+    Car car4("Honda");
+    Car car5 = car4; // here copy constructor is invoked made by me
+
+    car1.output();
+    car2.output();
+    car3.output();
+    car4.output();
+    car5.output();
 }
