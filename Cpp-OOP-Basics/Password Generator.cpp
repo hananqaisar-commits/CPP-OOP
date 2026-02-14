25048eafc18136f64ba5f3c9037d7b8a0fdc2647
Hanan Qaisar <hananqaisar316@gmail.com>
2026-02-14 21:13:26 +0500

    Add animation

--- Cpp-OOP-Basics/Password Generator.cpp
+++ Cpp-OOP-Basics/Password Generator.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 #include <time.h>
 #include <stdlib.h>
+#include <windows.h>
+
 using namespace std;
 
+void loader()
+{
+    cout << "Generating strong password: ";
+    for (int i = 0; i < 3; i++)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            cout << ".";
+            Sleep(300);
+        }
+        cout << "\b\b\b\b";
+        cout << "    ";
+        cout << "\b\b\b\b";
+    }
+}
 int main()
 {
 
@@ -17,6 +34,7 @@ int main()
                    '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '~', '[', ']'};
 
     srand(time(NULL));
+
     int size = sizeof(list) / sizeof(list[0]);
 
     for (int i = 0; i < lenght; i++)
@@ -24,7 +42,8 @@ int main()
         int position = rand() % size;
         password = password + list[position]; // it is bcz password is string and in string characters are concatinated instead of arithimatic operations
     }
-    cout << "password :" << password << endl;
+    loader();
+    cout << "\n\nGenerated password is:" << password << endl;
 
     return 0;
 }
