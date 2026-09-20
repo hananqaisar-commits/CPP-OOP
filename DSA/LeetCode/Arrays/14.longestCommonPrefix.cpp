097431f64b3b37e0149c9dc123dd4aa693ae8bb9
Hanan Qaisar (Linux) <hananqaisar316@gmail.com>
2026-09-20 12:28:44 +0500

    Leetcode: longest common prefix from string of arrays problem

---
+++ DSA/LeetCode/Arrays/14.longestCommonPrefix.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    std::string longestCommonPrefix(std::vector<std::string> &strs)
+    {
+        std::string prefix = "";
+        for (int i = 0; i < strs[0].size(); i++)
+        {
+            char current = strs[0][i];            // took first char of first string
+            for (int j = 1; j < strs.size(); j++) // start from index 1 and end at last
+            {
+                if (i > strs[j].size() || current != strs[j][i]) // must check bounds of bounds of checking and target strings
+                {
+                    return prefix;
+                }
+            }
+            prefix += current;
+        }
+        return prefix;
+    }
+};
+int main()
+{
+    std::cout << "======Longest Common Prefix=======\n";
+
+    std::vector<std::string> words = {"Hello", "Hey", "Hew", "Hell"};
+    Solution *lcp = new Solution();
+    std::cout << lcp->longestCommonPrefix(words);
+}
